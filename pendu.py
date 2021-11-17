135940eac72f005203ceea4a8041b5313819d53d
CodingGlitch <47162074+CodingGlitch@users.noreply.github.com>
2021-11-17 19:38:17 -0300

    Commentaires

--- pendu.py
+++ pendu.py
@@ -2,23 +2,25 @@
 from random import randint
 import tkinter as tk
 
+#Vous allez voir beaucoup de self.<variable>. C'est parce que j'utilise une classe. C'est plus simple après pour le côté graphique
 
-class Pendu :
+class Pendu : #Ne vous embêter pas avec une classe. C'est juste pour le côté graphique.
     def __init__(self) :
-        self.essaies = 10
+        self.essaies = 10 #Le nombre d'essaies restant
 
-        file = open("words.txt", 'r', encoding='UTF-8')
+        file = open("words.txt", 'r', encoding='UTF-8') # On ouvre le fichier contenant les mots avec l'encodage UTF-8, sinon c'est pas bon
 
-        self.mots = []
+        self.mots = [] #La liste des mots
 
         for i in file :
-            self.mots.append(i.rstrip())
+            self.mots.append(i.rstrip()) #On rajoute chaque mots dans le fichier directement dans la liste (après avoir retiré les \n)
 
-        file.close()
+        file.close() #On ferme le fichier car on a finit
 
-        self.motC = self.mots[randint(0, len(self.mots)-1)]
-        self.lettresTrouves = []
-
+        self.motC = self.mots[randint(0, len(self.mots)-1)] #On choisit un mot aléatoire
+        self.lettresTrouves = [] #La liste pour les lettres trouvées
+        
+        #SAUTER CETTE PARTIE !!!! C'est le code graphique.
         self.win = False
         self.root = tk.Tk()
         self.root.title("Pendu")
@@ -46,14 +48,16 @@ class Pendu :
         self.congratsLabel = tk.Label(self.root, text = "")
         self.congratsLabel.grid(column = 0, row = 3, padx = 10, pady = 10)
 
-    def restart(self) :
-        self.win = False
+    def restart(self) : #On démarre une nouvelle partie
+        self.win = False #Comme on démarre une nouvelle partie, le joueur n'a pas encore gagné
         
-        i = randint(0, len(self.mots)-1)
+        #On choisit un nouveau mot (qui ne correspond pas au mot précédent)
+        i = randint(0, len(self.mots)-1) 
         while self.motC == self.mots[i] :
             i = randint(0, len(self.mots)-1)
         self.motC = self.mots[i]
         
+        #On remet tout les paramètres de la partie à 0 : les lettres trouvées, le nombre d'essais restant, le dessin du pendu...
         self.lettresTrouves = []
         self.essaies = 10
         self.canvas.delete('all')
@@ -62,39 +66,46 @@ class Pendu :
         self.congratsLabel.configure(text = "")
         self.root.update_idletasks()
 
-    def penduTick(self) :
+    def penduTick(self) : #Le code principale quand on clique sur le boutton Entrée
 
-        if self.essaies > 0 and not self.win:
+        if self.essaies > 0 and not self.win: #Si on a encore des essaies et qu'on n'a pas encore gagné
             
+            #On récupère ce que le joueur à entrée
             guess = self.guessEntry.get()
             self.guessEntry.delete(0, -1)
             
-            for c in guess.lower() :
+            for c in guess.lower() : #On vérifie si la lettre correspond à une lettre du mot (les lettres qui correspondent ne font pas perdre d'essaies)
+                #On peut rentré plusieurs lettres d'un coup: si le joueur rentre "abcdefg", sa va regarder pour toutes ces lettre si elle est dans le mot.
+                #On perd aussi plus d'essaies si on met plein de lettres qui ne sont pas dans le mot.
                 if c in self.motC.lower() :
-                    if c not in self.lettresTrouves :
+                    if c not in self.lettresTrouves : #Si la lettre est dans le mot, 
                         self.lettresTrouves.append(c)
-                else :
-                    self.essaies -= 1
+                else : 
+                    self.essaies -= 1 #Mauvaise lettre ? Un essaie en moins et on dessine le pendu un peu plus
                     self.drawNext()
                     
-                if self.essaies > 0 :
-                    complete = True
+                if self.essaies > 0 : #Si on a encore des essaies (donc on peut pas tricher en mettant toutes les lettres de l'alphabet d'un coup)
+                    complete = True #On vérifie si le mot est complété par le joueur
                     for c in self.motC.lower() :
                         if c not in self.lettresTrouves :
-                            complete = False
+                            complete = False #Si le mot ne l'est pas, on continue
 
-                    if complete :
+                    if complete : #Si le mot est complété, alors on annonce la victoire du joueur.
                         self.win = True
                         self.congratsLabel.configure(text = "You won !")
                         break
-
+            
+            
+            #C'est juste pour mettre à jour l'interface graphique
+            #Pour le mot, on l'affiche avec des "_" pour les lettres inconnues
             newText = ""
             for i in self.motC :
-                if i.lower() in self.lettresTrouves :
-                    newText += i
+                if i.lower() in self.lettresTrouves : #Si la lettre du mot est connue
+                    newText += i #Alors on l'affiche directement
                 else :
-                    newText += "_"
+                    newText += "_" #Sinon on affiche un "_"
             
+            #Ici on met à jour le texte. Considéré ça comme des prints versions graphiques
             self.wordLabel.configure(text = newText)
             
             if self.essaies <= 0 :
@@ -104,11 +115,14 @@ class Pendu :
             self.triesLabel.configure(text = "You have " + str(self.essaies) + " tries left")
             self.root.update_idletasks()
 
-    def start(self) :
+    def start(self) : #Ne vous intéressez pas à cette fonction
         self.restart()
         self.root.mainloop()
 
     def drawNext(self):
+        #Commentaire anglais pour ceux qui comprennent
+        #On dessine le pendu en fonction du nombre d'essaies restants.
+        #Ne restez pas plantez ici à essayez de comprendre, je l'ai fait trop rapidement pour le faire de manière compréhensible
         #Time for a LOT of specific drawing calls
         self.canvas.delete('all')
         if self.essaies < 10 :
@@ -137,7 +151,9 @@ class Pendu :
 
 
 #Graphical stuff with tkinter
-
+#Et ici on lance le jeu. Oui c'est ici qu'il se lance. C'est pas avant.
+#Le code qu'il y a avant c'est juste pour pouvoir faire l'initialisation graphique et me faciliter la vie
+#Pour tout ce qui est dessin à l'écran. C'était soit ça, soit un code qui fait 30-40 lignes de plus pour rien.
 pendu = Pendu()
 pendu.start()
 
